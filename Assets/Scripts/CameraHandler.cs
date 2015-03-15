83ecf2841930b0e46d7b2fd610b08bee269e0683
Bryan327 <bshaner11@yahoo.com>
2015-03-14 20:52:54 -0400

    Fixed error with camera movement.

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 
 public class CameraHandler : MonoBehaviour {
-	public int currentEnemy = 0;
+	public float targetX = 0;
+	public float cameraX = 0;
 	public bool cameraIsMoving;
 
 	void Start () {
 		Camera.main.orthographicSize = 5;
-		StartCoroutine (moveCamera ());
+		//StartCoroutine (moveCamera ());
 	}
 
 	IEnumerator moveCameraToPos(Vector3 toPos, float zoom, float zoomPercent, float percent) {
@@ -19,15 +20,19 @@ public class CameraHandler : MonoBehaviour {
 			float destinationZoom = (zoom - currentZoom);
 			
 			destinationPos = Vector2.Scale (destinationPos, new Vector2 (1, 1));
+			Debug.Log ("X: " + destinationPos.x + ", Y: " + destinationPos.y);
 			
 			Camera.main.transform.localPosition += (percent * destinationPos);
+			cameraX = Camera.main.transform.localPosition.x;
 			Camera.main.orthographicSize += (zoomPercent * destinationZoom);
 
-			if (Camera.main.transform.localPosition.x <= (toPos.x + .1f) && Camera.main.transform.localPosition.x >= (toPos.x - .1f) && Camera.main.transform.localPosition.y <= (toPos.y + .1f) && Camera.main.transform.localPosition.y <= (toPos.y - .1f)) {
+			if (Camera.main.transform.localPosition.x <= (toPos.x + .05f) && Camera.main.transform.localPosition.x >= (toPos.x - .05f) && Camera.main.transform.localPosition.y <= (toPos.y + .05f) && Camera.main.transform.localPosition.y >= (toPos.y - .05f)) {
+				Camera.main.transform.localPosition.Set(toPos.x, toPos.y, -10f);
 				cameraIsMoving = false;
 			}
 			yield return null;
 		}
+		cameraX = Camera.main.transform.localPosition.x;
 	}
 
 	// Update is called once per frame
@@ -37,17 +42,21 @@ public class CameraHandler : MonoBehaviour {
 
 	IEnumerator moveCamera() {
 		GameObject target = (GameObject) CharacterHandler.enemies[0];
+		targetX = target.transform.localPosition.x;
 		StartCoroutine (moveCameraToPos (target.transform.localPosition, 4, .1f, .05f));
-		yield return new WaitForSeconds (2.0f);
+		yield return new WaitForSeconds (5.0f);
 		target = (GameObject) CharacterHandler.enemies[1];
+		targetX = target.transform.localPosition.x;
 		StartCoroutine (moveCameraToPos (target.transform.localPosition, 4, .1f, .05f));
-		yield return new WaitForSeconds (2.0f);
+		yield return new WaitForSeconds (5.0f);
 		target = (GameObject) CharacterHandler.enemies[2];
+		targetX = target.transform.localPosition.x;
 		StartCoroutine (moveCameraToPos (target.transform.localPosition, 4, .1f, .05f));
-		yield return new WaitForSeconds (2.0f);
+		yield return new WaitForSeconds (5.0f);
 		target = (GameObject) CharacterHandler.enemies[3];
+		targetX = target.transform.localPosition.x;
 		StartCoroutine (moveCameraToPos (target.transform.localPosition, 4, .1f, .05f));
-		yield return new WaitForSeconds (2.0f);
+		yield return new WaitForSeconds (5.0f);
 	}
 
 	void wait(float time) {
