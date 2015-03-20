92ce60147112c4d075b695abe60e18446e7358b4
Bryan327 <bshaner11@yahoo.com>
2015-03-20 00:04:21 -0400

    Tested ideas for location slots.

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -2,60 +2,36 @@
 using System.Collections;
 
 public class GameHandler : MonoBehaviour {
-	public static bool canAdvanceState = true;
+	public static bool canInteract = true;
 	public static int numberOfPlayers;
 	public static int currentPlayer;
 	public Player p;
 	public GameObject[] locationPrefabs;
-	public const int KEEP = 6;
+	public Location[] locations;
 
 	// Use this for initialization
 	void Start () {
 		QualitySettings.vSyncCount = 0;
 		Application.targetFrameRate = 60;
 		numberOfPlayers = 1;
-		p = new Player ();
+		canInteract = true;
+		locations = new Location[6];
 
 		GameObject instance = Instantiate (locationPrefabs[0], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+		locations [0] = new Location(instance);
 		instance = Instantiate (locationPrefabs[1], new Vector3(-10, 3, 0), Quaternion.identity) as GameObject;
+		locations [1] = new Location(instance);
 		instance = Instantiate (locationPrefabs[2], new Vector3(0, 8, 0), Quaternion.identity) as GameObject;
+		locations [2] = new Location(instance);
 		instance = Instantiate (locationPrefabs[3], new Vector3(10, 3, 0), Quaternion.identity) as GameObject;
+		locations [3] = new Location(instance);
 		instance = Instantiate (locationPrefabs[4], new Vector3(-5, -8, 0), Quaternion.identity) as GameObject;
+		locations [4] = new Location(instance);
 		instance = Instantiate (locationPrefabs[5], new Vector3(5, -8, 0), Quaternion.identity) as GameObject;
+		locations [5] = new Location(instance);
 
-
+		p = new Player (locations[0].loc);
 	}
-
-//	public void moveToLocation1() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("Location1Object").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
-//	public void moveToLocation2() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("Location2Object").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
-//	public void moveToLocation3() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("Location3Object").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
-//	public void moveToLocation4() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("Location4Object").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
-//	public void moveToLocation5() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("Location5Object").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
-//	public void moveToKeep() {
-//		for (int i = 0; i < players.Length; i++) {
-//			players[i].moveToPos (GameObject.Find ("KeepObject").transform.localPosition + new Vector3(players[i].positionOffset, -1, 0));
-//		}
-//	}
 
 	// Update is called once per frame
 	void Update () {
@@ -64,37 +40,61 @@ public class GameHandler : MonoBehaviour {
 
 	IEnumerator checkForMoveInput() {
 		if (Input.GetMouseButtonDown (0)) {
-			//Debug.Log ("Mouse is down");
-			GameObject[] loc = GameObject.FindGameObjectsWithTag ("Location");
+			Debug.Log ("Mouse is down");
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction);
-			foreach (var l in loc) {
+			for (int i = 0; i < 6; i++) {
 				if (hit != null & hit.collider != null) {
-					if (hit.collider.gameObject == l) {
-						//p.g.transform.localPosition = l.transform.localPosition;
-							yield return StartCoroutine (movePlayer (l.transform.localPosition, .05f));
+					Debug.Log ("hit: " + hit.collider.gameObject + ", loc: " + locations[i].loc);
+					if (hit.collider.gameObject == locations[i].loc && canInteract) {
+						canInteract = false;
+						yield return StartCoroutine (movePlayer (locations[i], .1f));
 					}
 				}
 			}
 		}
 		yield return null;
 	}
-	IEnumerator movePlayer(Vector3 toPos, float percent) {
+	IEnumerator movePlayer(Location l, float percent) {
+		int firstSlot = l.getFirstFreeSlot ();
+		l.setSlotToUsed (firstSlot);
+		GameObject targetSlot = l.loc.transform.Find ("Slot 1").gameObject;
 		p.destinationReached = false;
+
+		switch (firstSlot) {
+		case 0:
+			targetSlot = l.loc.transform.Find ("Slot 1").gameObject;
+			break;
+		case 1:
+			targetSlot = l.loc.transform.Find ("Slot 2").gameObject;
+			break;
+		case 2:
+			targetSlot = l.loc.transform.Find ("Slot 3").gameObject;
+			break;
+		case 3:
+			targetSlot = l.loc.transform.Find ("Slot 4").gameObject;
+			break;
+		}
+
+		Debug.Log ("Moving");
+
 		while (!p.destinationReached) {
-			Vector3 dest = toPos - p.g.transform.localPosition;
+			Vector3 dest = targetSlot.transform.position - p.g.transform.localPosition;
+
+			Debug.Log ("target: " + targetSlot.transform.position.ToString () + ", current: " + p.g.transform.localPosition.ToString ());
 			
 			dest = Vector3.Scale (dest, new Vector3 (1, 1, 0));
 
 			p.g.transform.localPosition += (percent * dest);
 
-			if (p.g.transform.localPosition.x <= (toPos.x + .05f) && p.g.transform.localPosition.x >= (toPos.x - .05f) && p.g.transform.localPosition.y <= (toPos.y + .05f) && p.g.transform.localPosition.y >= (toPos.y - .05f)) {
-				p.g.transform.localPosition.Set(toPos.x, toPos.y, -10f);
+			if (p.g.transform.localPosition.x <= (targetSlot.transform.position.x + .05f) && p.g.transform.localPosition.x >= (targetSlot.transform.position.x - .05f) && p.g.transform.localPosition.y <= (targetSlot.transform.position.y + .05f) && p.g.transform.localPosition.y >= (targetSlot.transform.position.y - .05f)) {
+				p.g.transform.localPosition.Set(targetSlot.transform.position.x, targetSlot.transform.position.y, -10f);
 				p.destinationReached = true;
+				canInteract = true;
+				Debug.Log ("Has stopped moving");
 			}
 			yield return null;
 		}
-
-
 	}
+	
 }
---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location : MonoBehaviour {
+
+	public GameObject loc;
+	public bool[] slotIsFree;
+
+	public Location(GameObject obj) {
+		loc = obj;
+		slotIsFree = new bool[4];
+
+		for (int i = 0; i < 4; i++) {
+			slotIsFree[i] = true;
+		}
+	}
+
+	public void setSlotToUsed(int i) {
+		slotIsFree [i] = false;
+	}
+
+	public void setSlotToFree(int i) {
+		slotIsFree [i] = true;
+	}
+
+	public int getFirstFreeSlot() {
+		int slot = 0;
+		for (int i = 0; i < 4; i++) {
+			if (slotIsFree[i] == true) {
+				slot = i;
+				break;
+			}
+		}
+		Debug.Log ("Slot: " + slot);
+		return slot;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
