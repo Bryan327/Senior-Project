ab5b2d4cba5e2e1a710387059e0cdbcb4b551452
dsilva609 <dsilva609@gmail.com>
2015-03-21 18:07:15 -0400

    trimmed the fat for camerahandler

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -1,128 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraHandler : MonoBehaviour {
-	public float targetX = 0;
-	public float cameraX = 0;
-	public bool cameraIsMoving;
+public class CameraHandler : MonoBehaviour 
+{
+	private float _targetX = 0;
+	private float _cameraX = 0;
+	private bool _cameraIsMoving;
 
-	void Start () {
+	void Start ()
+	{
 		//Camera.main.orthographicSize = 5;
 		//StartCoroutine (moveCamera ());
 	}
 
-	public IEnumerator moveCameraToPos(Vector3 toPos, float zoom, float zoomPercent, float percent) {
-		cameraIsMoving = true;
-		while (cameraIsMoving) {
-			Vector3 currentPos = Camera.main.transform.localPosition;
-			float currentZoom = Camera.main.orthographicSize;
-			Vector3 destinationPos = toPos - currentPos;
-			float destinationZoom = (zoom - currentZoom);
+	public IEnumerator moveCameraToPos(Vector3 toPos, float zoom, float zoomPercent, float percent)
+	{
+		this._cameraIsMoving = true;
+
+		Vector3 currentPos, destinationPos;
+		float currentZoom, destinationZoom;
+		//why were you constantly creating new variables in this loop?
+		while (this._cameraIsMoving)
+		{
+			currentPos = Camera.main.transform.localPosition;
+			currentZoom = Camera.main.orthographicSize;
+			destinationPos = toPos - currentPos;
+			destinationZoom = (zoom - currentZoom);
 			
 			destinationPos = Vector2.Scale (destinationPos, new Vector2 (1, 1));
 			Debug.Log ("X: " + destinationPos.x + ", Y: " + destinationPos.y);
 			
 			Camera.main.transform.localPosition += (percent * destinationPos);
-			cameraX = Camera.main.transform.localPosition.x;
+			this._cameraX = Camera.main.transform.localPosition.x;
 			Camera.main.orthographicSize += (zoomPercent * destinationZoom);
 
-			if (Camera.main.transform.localPosition.x <= (toPos.x + .05f) && Camera.main.transform.localPosition.x >= (toPos.x - .05f) && Camera.main.transform.localPosition.y <= (toPos.y + .05f) && Camera.main.transform.localPosition.y >= (toPos.y - .05f)) {
+			if (Camera.main.transform.localPosition.x <= (toPos.x + .05f) && Camera.main.transform.localPosition.x >= (toPos.x - .05f) && Camera.main.transform.localPosition.y <= (toPos.y + .05f) && Camera.main.transform.localPosition.y >= (toPos.y - .05f)) 
+			{
 				Camera.main.transform.localPosition.Set(toPos.x, toPos.y, -10f);
-				cameraIsMoving = false;
+				this._cameraIsMoving = false;
 			}
 			yield return null;
 		}
-		cameraX = Camera.main.transform.localPosition.x;
+		this._cameraX = Camera.main.transform.localPosition.x;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		checkInput ();
+	void Update() 
+	{
+		this.checkInput();
 	}
 
-	public void moveLocation1() {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location1Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.05f, 0.1f));
-		}
+	public void checkInput() 
+	{
+		//can probably be replaced with a switch just not sure what to switch on, Input.GetKeyDown(KeyCode.Alpha1?
+		if (Input.GetKeyDown(KeyCode.Alpha1) && !this._cameraIsMoving) 
+			this.MoveToLocation("Location1Object", 8, 0.1f, 0.1f);
+		
+		if (Input.GetKeyDown(KeyCode.Alpha2) && !this._cameraIsMoving) 
+			this.MoveToLocation("Location2Object", 8, 0.1f, 0.1f);
+
+		if (Input.GetKeyDown(KeyCode.Alpha3) && !this._cameraIsMoving) 
+			this.MoveToLocation("Location3Object", 8, 0.1f, 0.1f);
+
+		if (Input.GetKeyDown(KeyCode.Alpha4) && !this._cameraIsMoving) 
+			this.MoveToLocation("Location4Object", 8, 0.1f, 0.1f);
+
+		if (Input.GetKeyDown(KeyCode.Alpha5) && !this._cameraIsMoving) 
+			this.MoveToLocation("Location5Object", 8, 0.1f, 0.1f);
+
+		if (Input.GetKeyDown(KeyCode.Alpha6) && !this._cameraIsMoving) 
+			this.MoveToLocation("KeepObject", 12, 0.1f, 0.1f);
 	}
 
-	public void moveLocation2() {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location2Object").transform.localPosition;
+	private void MoveToLocation(string locationName, float x, float y, float z)
+	{
+		if (!this._cameraIsMoving) 
+		{
+			Vector3 target = GameObject.Find (locationName).transform.localPosition;
 			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-
-	}
-
-	public void moveLocation3() {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location3Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-
-	}
-
-	public void moveLocation4() {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location4Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-
-	}
-
-	public void moveLocation5() {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location5Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-
-	}
-
-	public void moveKeep () {
-		if (!cameraIsMoving) {
-			Vector3 target = GameObject.Find ("KeepObject").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 12, 0.1f, 0.1f));
-		}
-
-	}
-
-	public void checkInput() {
-		if (Input.GetKeyDown(KeyCode.Alpha1) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location1Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha2) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location2Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha3) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location3Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha4) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location4Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha5) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("Location5Object").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 8, 0.1f, 0.1f));
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha6) && !cameraIsMoving) {
-			Vector3 target = GameObject.Find ("KeepObject").transform.localPosition;
-			target = Vector3.Scale(target, new Vector3(1, 1, 0));
-			StartCoroutine (moveCameraToPos (target, 12, 0.1f, 0.1f));
+			StartCoroutine (moveCameraToPos (target, x, y, z));
 		}
 	}
 }
